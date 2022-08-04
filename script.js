3b420397cff31b99ef7d28db2d4504b91d4978e0
Flavio Silva <flaviosp15@outlook.com>
2022-08-04 15:15:42 -0300

    Adding event listener to small devices

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ const spanDate = document.querySelector('.span__date');
 const today = new Date();
 
 /* =========================== FUNCTIONS =========================== */
+const verifyValidInput = function () {
+  const splitEventDate = inputDate.value.split('-');
+  const currentDateInMilliseconds = today.getTime();
+  eventDay = splitEventDate[2];
+  eventMonth = splitEventDate[1];
+  eventYear = splitEventDate[0].slice(0, 1) === '0' ? null : splitEventDate[0];
+  eventDateInMilliseconds = new Date(`${eventMonth} ${eventDay}, ${eventYear} 00:00:00`).getTime();
+  if (eventDateInMilliseconds > currentDateInMilliseconds) validInput();
+  else if (eventDateInMilliseconds < currentDateInMilliseconds) invalidInput();
+  else noInput();
+};
 const noInput = function () {
   invalidIcon.classList.remove('invalid');
   validIcon.classList.remove('valid');
@@ -32,32 +43,31 @@ const validInput = function () {
   boxInput.classList.add('valid');
   btnUpdate.disabled = false;
 };
-const numberBelow10 = function (number) {
+const formatNumberLessThan10 = function (number) {
   return number < 10 ? `0${number}` : number;
 };
-const timeExpired = function () {
+const expiredTime = function () {
   daysText.textContent = 'Contagem';
   timeText.textContent = 'Finalizada!';
 };
 const displayCountdown = function (days, hours, minutes, seconds) {
-  daysText.textContent = `${days} dia${days > 1 ? 's' : ''}`;
-  timeText.textContent = `${numberBelow10(hours)}:${numberBelow10(minutes)}:${numberBelow10(seconds)}`;
+  const formatNumber = /\B(?=(\d{3})+(?!\d))/g;
+  daysText.textContent = `${days.toString().replace(formatNumber, '.')} dia${days > 1 ? 's' : ''}`;
+  timeText.textContent = `${formatNumberLessThan10(hours)}:${formatNumberLessThan10(minutes)}:${formatNumberLessThan10(seconds)}`;
 };
 
 noInput();
 
 /* =========================== LISTENERS =========================== */
 inputDate.addEventListener('keyup', function () {
-  const splitEventDate = this.value.split('-');
-  const millisecondsNow = today.getTime();
-  eventDay = splitEventDate[2];
-  eventMonth = splitEventDate[1];
-  eventYear = splitEventDate[0].slice(0, 1) === '0' ? null : splitEventDate[0];
-  eventDateInMilliseconds = new Date(`${eventMonth} ${eventDay}, ${eventYear} 00:00:00`).getTime();
-  if (eventDateInMilliseconds > millisecondsNow) validInput();
-  else if (eventDateInMilliseconds < millisecondsNow) invalidInput();
-  else noInput();
+  verifyValidInput();
 });
+
+if (window.innerWidth < 768) {
+  inputDate.addEventListener('click', function () {
+    verifyValidInput();
+  });
+}
 
 btnUpdate.addEventListener('click', function (e) {
   e.preventDefault();
@@ -71,15 +81,17 @@ btnUpdate.addEventListener('click', function (e) {
 /* =========================== RUN COUNTDOWN TIMER =========================== */
 setInterval(function () {
   const now = new Date().getTime(),
-    differenceBeetwenDates = eventDate - now,
-    daysToEventDate = Math.floor(differenceBeetwenDates / (1000 * 60 * 60 * 24)),
-    hoursToEventDate = Math.floor((differenceBeetwenDates % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    minutesToEventDate = Math.floor((differenceBeetwenDates % (1000 * 60 * 60)) / (1000 * 60)),
-    secondsToEventDate = Math.floor((differenceBeetwenDates % (1000 * 60)) / 1000);
+    differenceInMilliseconds = eventDate - now,
+    daysToEventDate = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24)),
+    hoursToEventDate = Math.floor((differenceInMilliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    minutesToEventDate = Math.floor((differenceInMilliseconds % (1000 * 60 * 60)) / (1000 * 60)),
+    secondsToEventDate = Math.floor((differenceInMilliseconds % (1000 * 60)) / 1000),
+    isUndefined = eventDate === undefined,
+    areMillisecondsBelow0 = differenceInMilliseconds < 0;
 
-  if (differenceBeetwenDates <= 0) {
-    timeExpired();
-  } else if (eventDate === undefined) {
+  if (areMillisecondsBelow0) {
+    expiredTime();
+  } else if (isUndefined) {
     return null;
   } else {
     displayCountdown(daysToEventDate, hoursToEventDate, minutesToEventDate, secondsToEventDate);
